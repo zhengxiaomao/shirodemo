946757ba5face43817f11edef597114d50365d37
mac <976132880@qq.com>
2021-10-24 10:41:18 +0800

    Initial commit

---
+++ src/main/java/com/example/shirodemo/bean/Permissions.java
@@ -0,0 +1,11 @@
+package com.example.shirodemo.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Permissions {
+    private String id;
+    private String permissionsName;
+}
---
+++ src/main/java/com/example/shirodemo/service/LoginService.java
@@ -0,0 +1,9 @@
+package com.example.shirodemo.service;
+
+import com.example.shirodemo.bean.User;
+
+public interface LoginService {
+
+    public User getUserByName(String getMapByName);
+
+}
